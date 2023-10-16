5ea84bbd89a76f3184217767d92760a1a7d94d9c
Emre-11 <144242206+Emre-11@users.noreply.github.com>
2023-10-15 22:07:22 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,15 @@
+import math
+
+Dvalue = (input("Enter diameter:"))
+if Dvalue.isnumeric():
+
+    diameter = float(Dvalue)
+    radius = diameter/2
+    circumference = diameter*(math.pi)
+    area = (math.pi)*radius**2
+
+    print("Circumference is "+str("%.2f" %circumference))
+
+    print("Area is "+str("%.2f" %area))
+else: print("Please enter a numeric value")
 
